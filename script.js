31b28d331fbe638ae1ddf7294c7e0ad8abd6c94d
Zunairviqar <66274735+Zunairviqar@users.noreply.github.com>
2020-06-10 00:33:49 +0500

    Team 2 images running

--- script.js
+++ script.js
@@ -13,12 +13,30 @@ $(document).ready(function(){
 // https://www.w3schools.com/howto/howto_js_typewriter.asp
 var i = 0;
 var txt = 'Zunair Viqar, Sophomore, From Pakistan, Enjoys chicken dinners in PUBG.'; /* The text */
-var speed = 150; /* The speed/duration of the effect in milliseconds */
-
-function typeWriter() {
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+console.log(txt.length);
+$(document).ready(function typeWriter1() {
   if (i < txt.length) {
-    document.getElementById("demo").innerHTML += txt.charAt(i);
+    document.getElementById("image1").innerHTML += txt.charAt(i);
     i++;
-    setTimeout(typeWriter, speed);
+    setTimeout(typeWriter1, speed);
   }
-}
+});
+
+setTimeout(function () {
+    $("#SecondImage").show()
+}, 4000);
+
+var j = 0;
+var txt2 = 'Hasin, Sophomore, From Pakistan, Enjoys chicken dinners in PUBG.'; /* The text */
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+setTimeout(function () {
+    $("#SecondImage").ready(function typeWriter2() {
+      if (j < txt2.length) {
+        document.getElementById("image2").innerHTML += txt2.charAt(j);
+        j++;
+        setTimeout(typeWriter2, speed);
+      }
+    });
+}, 4000);
+// console.log(txt2.length);
