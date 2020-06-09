50c77af2169683613309daf54eac08d05e4ad816
Zunairviqar <66274735+Zunairviqar@users.noreply.github.com>
2020-06-09 23:25:13 +0500

    Team Page

--- script.js
+++ script.js
@@ -10,3 +10,15 @@ $(document).ready(function(){
     $(".Shutuptext").fadeOut("fast");
   });
 });
+// https://www.w3schools.com/howto/howto_js_typewriter.asp
+var i = 0;
+var txt = 'Zunair Viqar, Sophomore, From Pakistan, Enjoys chicken dinners in PUBG.'; /* The text */
+var speed = 150; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
