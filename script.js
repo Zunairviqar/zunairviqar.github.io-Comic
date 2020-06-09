639f571c3d71cbb1a981ef81ce3960f88d9710ea
Zunairviqar <66274735+Zunairviqar@users.noreply.github.com>
2020-06-09 21:41:24 +0500

    Blinking Update

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $(".bg").click(function(){
+    $(".vertical-menu").fadeIn(1000);
+  });
+});
+
+$(document).ready(function(){
+  $(".bg").click(function(){
+    $(".Shutuptext").fadeOut();
+  });
+});
