d3f31fa263466ac377a52e91b153bff64b3c24ff
Zunairviqar <66274735+Zunairviqar@users.noreply.github.com>
2020-06-10 03:48:06 +0500

    Team Page All Pictures

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ setTimeout(function () {
 }, 4000);
 
 var j = 0;
-var txt2 = 'Hasin, Sophomore, From Pakistan, Enjoys chicken dinners in PUBG.'; /* The text */
+var txt2 = 'Tonya, Sophomore, From Pakistan, Enjoys chicken dinners in PUBG.'; /* The text */
 var speed = 50; /* The speed/duration of the effect in milliseconds */
 setTimeout(function () {
     $("#SecondImage").ready(function typeWriter2() {
@@ -40,3 +40,41 @@ setTimeout(function () {
     });
 }, 4000);
 // console.log(txt2.length);
+
+setTimeout(function () {
+    $("#ThirdImage").show()
+}, 8000);
+
+var k = 0;
+var txt3 = 'Yahia, Sophomore, From Pakistan, Enjoys chicken dinners in PUBG.'; /* The text */
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+setTimeout(function () {
+    $("#ThirdImage").ready(function typeWriter3() {
+      if (k < txt3.length) {
+        document.getElementById("image3").innerHTML += txt3.charAt(k);
+        k++;
+        setTimeout(typeWriter3, speed);
+      }
+    });
+}, 8000);
+
+setTimeout(function () {
+    $("#FourthImage").show()
+}, 12500);
+
+var l = 0;
+var txt4 = 'Yahia, Sophomore, From Pakistan, Enjoys chicken dinners in PUBG.'; /* The text */
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+setTimeout(function () {
+    $("#FourthImage").ready(function typeWriter4() {
+      if (l < txt4.length) {
+        document.getElementById("image4").innerHTML += txt4.charAt(l);
+        l++;
+        setTimeout(typeWriter4, speed);
+      }
+    });
+}, 12500);
+
+setTimeout(function () {
+    $(".UnderlineEffect").show()
+}, 16500);
